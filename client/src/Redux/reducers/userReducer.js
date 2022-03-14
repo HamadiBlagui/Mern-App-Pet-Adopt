84eb168768d212fd0adbed4e9c29ab1379a92f88
HamadiBlagui <90401108+HamadiBlagui@users.noreply.github.com>
2022-03-14 17:01:15 +0100

    front end first creation

---
+++ client/src/Redux/reducers/userReducer.js
@@ -0,0 +1,11 @@
+const initialeState={
+    User:{}
+}
+const userReducer =(state=initialeState, action)=>{
+    const{type,payload}= action
+        switch (type) {
+           
+            default:
+             return state
+        }
+}
---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
+import store from "./Redux/store"
+
+
+
+
+
+
+
+ReactDOM.render(
+  <BrowserRouter>
+  <Provider store={store}  >
+  <App />
+  </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
